5a0bc88110c5d14b484f79d752991ea8c25bbb02
phpiiper <plp@njit.edu>
2025-02-06 18:23:36 -0500

    - Fixed build (provider) seen as page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { Provider } from  "./provider";
+import { Provider } from  "@/components/provider";
 
 export default function App({ Component, pageProps }) {
   return <Provider> <Component {...pageProps} /> </Provider>;
