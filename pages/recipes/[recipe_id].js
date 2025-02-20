b3f701f2a52d21f19f8b37d70c366cf26fcc4e89
phpiiper <plp@njit.edu>
2025-02-19 20:41:19 -0500

    better mobile mode:  - added step/ingredient view buttons for mobile  - scaled down font sizes  - made smaller recipe scale for ingredient scaling

--- pages/recipes/[recipe_id].js
+++ pages/recipes/[recipe_id].js
@@ -9,10 +9,20 @@ import { useSession } from "next-auth/react"
 import EditIcon from '@mui/icons-material/Edit';
 import Icon from "@/components/Icon";
 import HomeIcon from "@mui/icons-material/Home";
+import TextField from "@mui/material/TextField";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
 const fetcher = (url) => fetch(url).then(res => res.json())
 export default function RecipePage() {
     const { status, data: sessionData } = useSession();
     const [sizing, setSizing] = useState(1);
+    const [visibleIngredient, setVisibleIngredient] = useState(true);
+    const [visibleSteps, setVisibleSteps] = useState(true);
     const r = useRouter();
     const { recipe_id } = r.query;
     let { data, error } = useSWR(recipe_id ? `/api/getrecipe?id=${recipe_id}` : null, recipe_id ? fetcher : null)
@@ -27,14 +37,7 @@ export default function RecipePage() {
     let ig = data.ingredients;
     const sizingArray = [0.25,0.5,0.75,1,1.25,1.5,1.75,2]
     let compactClass = getComputedStyle(document.documentElement).getPropertyValue('--compactSize');
-    function handleSizing(event){
-        let prev_btn = document.getElementById("checkedSizingBtn");
-        if (prev_btn){prev_btn.id = null}
-        let btn = event.currentTarget;
-        if (btn.nodeName === "BUTTON"){
-            btn.id = "checkedSizingBtn";
-            setSizing(btn.dataset.data)
-        }
+    function handleSizing(){
     }
     let time = "";
     if (data.time){
@@ -61,17 +64,50 @@ export default function RecipePage() {
                     {data.servings ? <span className={"recipe-serving-size"}>{!isNaN(data.servings) ? JSON.parse(data.servings)*sizing : 0} Serving{Number(data.servings) * sizing > 1 ? 's' : ''} </span> : <></>}
                     <span className={"recipe-time"}>{time}</span>
                 </div>
-                <div className={"sizing"}>
-                    <span className={"sizing-text"}>INGREDIENT SCALE</span>
-                    {sizingArray.map(x => <button key={"sizing-btn-"+x} id={x===1 ? "checkedSizingBtn" : ""} className={"sizing-button"} data-data={x} onClick={handleSizing}>{x.toFixed(2) +"x"}</button> )}
+                <div style={{paddingTop: "1rem"}}>
+                    <TextField
+                        id="sizing"
+                        value={sizing}
+                        onChange={(event) => {
+                            setSizing(event.target.value)
+                        }}
+                        style={{ width: "4rem", minWidth: "8rem" }}
+                        inputProps={{
+                            'aria-label': 'sizing',
+                            'min': 0, 'step': 0.25
+                        }}
+                        label="Ingredient Scale"
+                        type={"number"}
+                    />
+                    {/*   sizingArray.map(x => <button key={"sizing-btn-"+x} id={x===1 ? "checkedSizingBtn" : ""} className={"sizing-button"} data-data={x} onClick={handleSizing}>{x.toFixed(2) +"x"}</button> ) */}
                 </div>
             </div>
+            <div className={"mobile-only-viewer container"}>
+                <Icon
+                    btnClass={"const"}
+                    children={<VisibilityIcon />}
+                    btnText={"Ingredients"}
+                    clickEvent={() => {
+                        setVisibleIngredient(true)
+                        setVisibleSteps(false)
+                    }}
+                />
+                <Icon
+                    btnClass={"const"}
+                    children={<VisibilityIcon />}
+                    btnText={"Steps"}
+                    clickEvent={() => {
+                        setVisibleSteps(true)
+                        setVisibleIngredient(false)
+                    }}
+                />
+            </div>
             <div className={"bot"}>
-                <div id={"steps-div"}>
+                <div id={"steps-div"} style={{display: visibleSteps ? "flex" : "none"}}>
                     <h1>STEPS</h1>
                     {data.steps.map((x,index) => <Step step={x} num={index+1} key={data.id + "-s-" + index} />) }
                 </div>
-                <div id={"ingredients-div"}>
+                <div id={"ingredients-div"} style={{display: visibleIngredient ? "flex" : "none"}}>
                     <h1>INGREDIENTS</h1>
                     {ig.map((x,index) => <Ingredient ingredient={x} key={data.id + "-i-" + index} sizing={sizing} ingIndex={index}/>) }
                 </div>
